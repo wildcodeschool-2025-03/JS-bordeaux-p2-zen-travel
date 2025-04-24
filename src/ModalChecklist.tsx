b2eefce498c6e2b04a0b38ea34284f9e39abe72b
ThomasCH19 <thomas.chategnier@yahoo.com>
2025-04-24 14:37:00 +0200

    add ModalChecklist

--- src/ModalChecklist.tsx
+++ src/ModalChecklist.tsx
@@ -2,23 +2,26 @@ import "./ModalChecklist.css";
 import { useEffect, useState } from "react";
 
 function ModalChecklist() {
-	const [countriesName, setCountriesName] = useState([]);
+	const [countriesInfo, setCountriesInfo] = useState([]);
 
 	useEffect(() => {
-		fetch("https://restcountries.com/v3.1/all")
+		fetch(
+			"https://my-json-server.typicode.com/wildcodeschool-2025-03/JS-bordeaux-p2-api-zen-travel/db",
+		)
 			.then((response) => response.json())
-			.then((data) => setCountriesName(data));
+			.then((data) => setCountriesInfo(data));
 	}, []);
 
 	return (
 		<>
-			{countriesName.length > 18 ? (
-				<h1>
-					🎒 Préparer votre voyage pour le {countriesName[18].name.common}
-				</h1>
+			{countriesInfo.length > 2 ? (
+				<h1>🎒 Préparer votre voyage pour le {countriesInfo[1].name.common}</h1>
 			) : (
 				<h1>Chargement du pays...</h1>
 			)}
+			<ul>
+				<input type="checkbox" />
+			</ul>
 		</>
 	);
 }
