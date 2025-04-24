9186dd569edfbf3bb047c3c85a938e8c26f26ff0
MICKAEL_MALLINGER <mallingermickael@sfr.fr>
2025-04-24 14:51:19 +0200

    created mainCard.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,41 @@
 import "./App.css";
+import MainCard from "./components/MainCard";
+import { useEffect, useState } from "react";
 
 function App() {
-	const name = "toto";
-	const text = "Welcome";
+	const [country, setCountry] = useState(null);
+
+	useEffect(() => {
+		fetch("https://restcountries.com/v3.1/all")
+			.then((response) => response.json())
+			.then((data) => setCountry(data[50]))
+			.catch((err) => console.error(err));
+	}, []);
+
 	return (
 		<>
-			{text} {name}
+			<div>
+				{country ? (
+					<div>
+						<img
+							className="flagCountries"
+							src={country.flags.png}
+							alt={`Flag of ${country.name.common}`}
+							width="150"
+						/>
+						<MainCard />
+						<h2>{country.name.common}</h2>
+						<p>Capital: {country.capital?.[0]}</p>
+						<p>Region: {country.region}</p>
+						<p>Population: {country.population.toLocaleString()}</p>
+					</div>
+				) : (
+					<p>Désolé,le pays selectionné n'est pas dans la liste!</p>
+				)}
+			</div>
+			<div>
+				<button type="button">Pack ta valise</button>
+			</div>
 		</>
 	);
 }
