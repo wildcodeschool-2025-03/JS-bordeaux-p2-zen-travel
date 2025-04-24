dc596e15cba49f5311d76c73b3ac0cd6fbefdb54
Imane Bichon <imane.bichon@icloud.com>
2025-04-24 11:46:18 +0200

    add slash to the path of interactive-map

--- src/router.tsx
+++ src/router.tsx
@@ -12,7 +12,7 @@ const router = createBrowserRouter([
 				element: <Home />,
 			},
 			{
-				path: "interactive-map",
+				path: "/interactive-map",
 				element: <InteractiveMap />,
 			},
 		],
