3cc3dc136879e1eeee635fa6815219b33c645497
ThomasCH19 <thomas.chategnier@yahoo.com>
2025-05-05 17:54:01 +0200

    modified gastronomy component

---
+++ src/components/Gastronomy/Gastronomy.tsx
@@ -0,0 +1,45 @@
+import "./Gastronomy.css";
+import { useEffect, useState } from "react";
+
+function Gastronomy() {
+	const [countriesInfo, setCountriesInfo] = useState([]);
+
+	useEffect(() => {
+		fetch(
+			"https://my-json-server.typicode.com/wildcodeschool-2025-03/JS-bordeaux-p2-api-zen-travel/db",
+		)
+			.then((response) => response.json())
+			.then((data) => setCountriesInfo(data))
+			.catch((err) => console.error("Erreur de fetch :", err));
+	}, []);
+
+	if (
+		!countriesInfo ||
+		!countriesInfo.countries ||
+		countriesInfo.countries.length < 2
+	) {
+		return <h1>Chargement du pays...</h1>;
+	}
+	const country = countriesInfo.countries[1];
+
+	return (
+		<article className="gastronomy-body">
+			<h1>Plats Typiques</h1>
+			<div className="dishes">
+				{country.typical_dishes.slice(0, 3).map((dish) => (
+					<div key={dish.id} className="dish-card">
+						<img
+							src={`src/img/${dish.picture}`}
+							alt="typical-dish-picture"
+							id={`dish-${dish.id}`}
+						/>
+						<h2>{dish.name}</h2>
+						<p>{dish.description}</p>
+					</div>
+				))}
+			</div>
+		</article>
+	);
+}
+
+export default Gastronomy;
