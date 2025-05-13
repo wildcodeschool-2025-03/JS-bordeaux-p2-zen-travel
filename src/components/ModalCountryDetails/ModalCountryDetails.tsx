cb7d960cda1bec55b22e12fb8cc1b5fabb609aca
Imane Bichon <imane.bichon@icloud.com>
2025-05-14 01:15:34 +0200

    add Climat componnent

--- src/components/ModalCountryDetails/ModalCountryDetails.tsx
+++ src/components/ModalCountryDetails/ModalCountryDetails.tsx
@@ -167,7 +167,7 @@ function ModalCountryDetails({
 
 					{activTab === "Gastronomie" && <Gastronomy />}
 
-					{activTab === "Climat" && <Climat />}
+					{activTab === "Climat" && <Climat country={country} />}
 
 					{activTab === "Tips" && <Tips />}
 
